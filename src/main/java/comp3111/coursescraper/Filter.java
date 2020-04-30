fd9fb52e945a657a63925b4713ad5ffe9492b132
tamww <jackytam97107@gmail.com>
2020-05-01 02:57:46 +0800

    s

--- src/main/java/comp3111/coursescraper/Filter.java
+++ src/main/java/comp3111/coursescraper/Filter.java
@@ -28,18 +28,20 @@ public class Filter {
     	CBList[9] = NExclBox;
     	CBList[10] = LabBox;
 	 * */
-	public List<Course> call_filter (CheckBox[] CBList, List<Course> input) {
-		List<Course> temp = input;
+	public List<Course> call_filter (Boolean [] CBList, List<Course> input) {
+		List<Course> temp = new ArrayList<Course>(input);
+
+
 		boolean flag = false;
-    	if(CBList[0].isSelected() && CBList[1].isSelected()) {
-    		temp = filtertime(temp, true, true);
+    	if((CBList[0]==true) && (CBList[1]==true)) {
+    		temp = new ArrayList<Course>(filtertime(temp, true, true));
     		flag = true;
-//    		System.out.println("captured");
-    	}else if (CBList[0].isSelected()) {
-    		temp = filtertime(temp, true, false);
+    		System.out.println("captured");
+    	}else if (CBList[0]==true && CBList[1]==false) {
+    		temp = new ArrayList<Course>(filtertime(temp, true, false));
     		flag = true;
-    	}else if (CBList[1].isSelected()){
-    		temp = filtertime(temp, false, true);
+    	}else if (CBList[0]==false && CBList[1]==true){
+    		temp = new ArrayList<Course>(filtertime(temp, false, true));
     		flag = true;
     	}
     	boolean mon_flag = false;
@@ -50,31 +52,31 @@ public class Filter {
     	boolean sat_flag = false;
     	boolean call_filter_date = false;
     	
-    	if (CBList[2].isSelected()) {
+    	if (CBList[2]==true) {
     		mon_flag = true;
     		call_filter_date = true;
     	}
-    	if (CBList[3].isSelected()) {
+    	if (CBList[3]==true) {
     		tue_flag = true;
     		call_filter_date = true;
 
     	}
-    	if (CBList[4].isSelected()) {
+    	if (CBList[4]==true) {
     		wed_flag = true;
     		call_filter_date = true;
 
     	}
-    	if (CBList[5].isSelected()) {
+    	if (CBList[5]==true) {
     		thur_flag = true;
     		call_filter_date = true;
 
     	}
-    	if (CBList[6].isSelected()) {
+    	if (CBList[6]==true) {
     		fri_flag = true;
     		call_filter_date = true;
 
     	}
-    	if (CBList[7].isSelected()) {
+    	if (CBList[7]==true) {
     		sat_flag = true;
     		call_filter_date = true;
 
@@ -84,17 +86,17 @@ public class Filter {
     		temp = filterdate(temp, mon_flag, tue_flag, wed_flag, thur_flag, fri_flag, sat_flag);
     	}
     	
-    	if (CBList[8].isSelected()) {
+    	if (CBList[8]==true) {
     		temp = filterCC(temp);
     		flag = true;
     	}
     	
-       	if (CBList[9].isSelected()) {
+       	if (CBList[9]==true) {
     		temp = filterEX(temp);
     		flag = true;
     	}
        	
-       	if (CBList[10].isSelected()) {
+       	if (CBList[10]==true) {
     		temp = filterLab(temp);
     		flag = true;
     	}
@@ -112,31 +114,33 @@ public class Filter {
 	public List<Course> filtertime(List<Course> input, boolean AMM, boolean PMM) {
 		System.out.println("captured2");
 
-		List<Course> result = new ArrayList<Course>();
+		List<Course> result = new ArrayList<Course>(input);
 		for (int i = 1; i < input.size(); i++) {
 			Course curr_course = input.get(i);
-			Course to_add = curr_course;
-			to_add.resetSlot();
+			Course to_add = new Course();
+			to_add.Clone(curr_course);
 			Set<String> section_flag = new HashSet<String>();
-			
 			Set<String> section_AMPM = new HashSet<String>();
 			Set<String> section_AM = new HashSet<String>();
 			Set<String> section_PM = new HashSet<String>();
+			System.out.println(curr_course.getTitle());
 			// get the related section code
-			for (int j = 1; j < curr_course.getNumSlots();j++) {
+			for (int j = 0; j < curr_course.getNumSlots();j++) {
 				Slot curr_slot = curr_course.getSlot(j);
 				if ((curr_slot.getStartHour()<=12)&&(curr_slot.getEndHour()>12)) {
 					section_AMPM.add(curr_slot.getSections());
 				}
 				if	((curr_slot.getStartHour()<=12)) {
 					section_AM.add(curr_slot.getSections());
+					System.out.println("captured3332");
+
 				}
 				if	((curr_slot.getStartHour()>12)) {
 					section_PM.add(curr_slot.getSections());
 				}
 			}
 			
-			if(AMM && PMM) {
+			if(AMM==true&& PMM==true) {
 				Set<String> intersection = new HashSet<String>(section_AM);
 				intersection.retainAll(section_PM);;
 				section_flag.addAll(section_AMPM);
@@ -148,10 +152,10 @@ public class Filter {
 			}
 	
 			// add the section to return result
-			for (int j = 1; j < curr_course.getNumSlots();j++) {
+			for (int j = 0; j < curr_course.getNumSlots();j++) {
 				Slot curr_slot = curr_course.getSlot(j);
 				if (section_flag.contains(curr_slot.getSections())) {
-					to_add.addSlot(curr_slot);
+					to_add.addSlot(curr_slot.clone());
 				}
 			}
 			// add the section 
@@ -174,7 +178,7 @@ public class Filter {
 
 	public List<Course> filterdate(List<Course> input, boolean flag_Mon, boolean flag_Tue, boolean flag_Wed, 
 			boolean flag_Thur, boolean flag_Fri, boolean flag_Sat) {
-		System.out.println("captured2");
+		System.out.println("captured3");
 
 		List<Course> result = new ArrayList<Course>();
 		for (int i = 1; i < input.size(); i++) {
@@ -190,7 +194,7 @@ public class Filter {
 			Set<String> section_Sat = new HashSet<String>();
 
 			// get the related section code
-			for (int j = 1; j < curr_course.getNumSlots();j++) {
+			for (int j = 0; j < curr_course.getNumSlots();j++) {
 				Slot curr_slot = curr_course.getSlot(j);
 				if (curr_slot.getDay() == 0) {
 					section_Mon.add(curr_slot.getSections());
@@ -253,7 +257,7 @@ public class Filter {
 			}
 			
 			// add the section to return result
-			for (int j = 1; j < curr_course.getNumSlots();j++) {
+			for (int j = 0; j < curr_course.getNumSlots();j++) {
 				Slot curr_slot = curr_course.getSlot(j);
 				if (section_flag.contains(curr_slot.getSections())) {
 					to_add.addSlot(curr_slot);
