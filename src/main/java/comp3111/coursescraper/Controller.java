cfb3beef519abd3b4953ea212cd81533f1018c85
zhou zhuo rui <zzhouba@connect.ust.hk>
2020-05-05 07:29:59 +0800

    I make changes on controller

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -1,6 +1,8 @@
 package comp3111.coursescraper;
 
 import java.awt.Checkbox;
+
+
 import java.awt.event.ActionEvent;
 import javafx.event.EventHandler;
 
@@ -15,6 +17,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.effect.ColorInput;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
@@ -49,6 +52,7 @@ import java.util.List;
 public class Controller  implements Initializable{
 	private static List<Course> myCourseList;
 	private List<Course> filterCourse;
+	private List<Course> drawCourse = new ArrayList<Course>();
 	private List<String> EnrolledCourse = new ArrayList<String>();
 	
 	
@@ -202,7 +206,7 @@ public class Controller  implements Initializable{
 //    private ObservableList<Courselist> tblist = FXCollections.observableArrayList();
     private ObservableList<Courselist> tblist = FXCollections.observableArrayList(item -> new javafx.beans.Observable[] {item.checkedProperty()});
 
-    
+    //After handlebox function
     @FXML
     void enrollmentUpdate() {
     	
@@ -214,15 +218,32 @@ public class Controller  implements Initializable{
     	/*
     	 * store the list of enrolled course
     	 * */
+    	
+    	// Checking and recording the enrollment status in the CourseListTable
+    	
+    	// Enrollmentupdate would also be called if selected
     	for (int i =0;i< CourseListTable.getItems().size();i++) {
     		String courseIDString = CourseListTable.getItems().get(i).getCourseCode() + "--" + CourseListTable.getItems().get(i).getSection();
     		if (CourseListTable.getItems().get(i).getEnroll().isSelected()) {
     			if(EnrolledCourse.contains(courseIDString) == false) {
     				EnrolledCourse.add(courseIDString);
+    				for (Course ftcurr : filterCourse) {
+    					String ftcurr_string = ftcurr.getTitle().split("\\ -")[0] + "--" + ftcurr.getSlot(i).getSectionCode();
+    					if (ftcurr_string == courseIDString) {
+    						drawCourse.add(ftcurr);
+    					}
+    				}
     			}
-    		}else {
+    		}
+    		else {
     			if(EnrolledCourse.contains(courseIDString) == true) {
     				EnrolledCourse.remove(courseIDString);
+    				for (Course ftcurr : filterCourse) {
+    					String ftcurr_string = ftcurr.getTitle().split("\\ -")[0] + "--" + ftcurr.getSlot(i).getSectionCode();
+    					if (ftcurr_string == courseIDString) {
+    						drawCourse.remove(ftcurr);
+    					}
+    				}
     			}
     		}
     	}   
@@ -237,10 +258,12 @@ public class Controller  implements Initializable{
 //    		tblist.add(new Courselist("N/A","N/A","N/A","N/A"));
 //    		return;
 //    	}
+
+    	/* Get num slooooooooooooooooooot!*/
     	
     	for (Course curr : filterCourse) {
     		Set<String> checkedID = new HashSet<String>();
-    		String [] titleL = curr.getTitle().split("\\ -");
+    		String [] titleL = curr.getTitle().split("\\ -"); 
     		
     		for(int i=0;i<curr.getNumSlots();i++) {
     			Courselist currCour = new Courselist(titleL[0], curr.getSlot(i).getSectionCode(), titleL[1], curr.getSlot(i).getInstructor());
@@ -251,13 +274,14 @@ public class Controller  implements Initializable{
     				currCour.getEnroll().setSelected(true);
     			}
     			
-    	    	for(int k = 0; k < EnrolledCourse.size(); k++) {
-    	    		System.out.println(EnrolledCourse.get(k));
-    	    	}
+//    	    	for(int k = 0; k < EnrolledCourse.size(); k++) {
+  //  	    		System.out.println(EnrolledCourse.get(k));
+    //	    	}
     			/* if this course section has not been added to the table list
     			 * add the course section the checkedID reference set
     			 * and add this course section to the table list to show it
     			 */
+    			// I don't need to understand this -Andrew
     			if(checkedID.contains(CourRefID) == false) {
     				checkedID.add(CourRefID);
     				tblist.add(currCour);
@@ -357,6 +381,7 @@ public class Controller  implements Initializable{
     	} 	
     	enrollmentUpdate();
     	updateList();
+    	drawtable();
     	
 
     	return;
@@ -437,6 +462,11 @@ public class Controller  implements Initializable{
         	// Debugging purpose
     		System.out.println("Finished subject search");
     		});
+    	
+    	// To draw the timetable if the enrollment is not yet implemented
+    	if (enrollbox.isEditable()==false) {
+    		drawtable();
+    	}
     	}
 
     
@@ -472,7 +502,10 @@ public class Controller  implements Initializable{
     
     @FXML
     void findInstructorSfq() {
-    	buttonInstructorSfq.setDisable(true);
+    	buttonInstructorSfq.setDisable(false);
+    	
+    	textAreaConsole.setText("Let's get started!");
+    	System.out.println("Would this show up?");
     }
 
     @FXML
@@ -498,18 +531,23 @@ public class Controller  implements Initializable{
     	else if (v.size() == 1 && v.get(0).getTitle().substring(0,3).equals("404"))  {
     		textAreaConsole.setText("Invalid URL: Page not found");
     	}
+    	
+    	// Scrape is successful and copy courses to myCourseList and display the output
     	else {
     		// Get search result statistics
     		myCourseList = v;
     		String info1 = "Total number of different Sections in this search: " + Section.getNumSections() + "\n";
     		String info2 = "Total number of Courses in this search: " + Course.getNumCourse() + "\n";
     		String info3 = "Instructor who has teaching assignment this term but does not need to teach at Tu 3:10pm: \n";
+    		
+    		//Print instructor info
     		List<String> nameList = Instructor.getInstructorNoTu1510();
     		for (String name : nameList) {
     			info3 += name + "\n";
     		}
     		textAreaConsole.setText(info1 + info2 + info3);
     		
+    		// Print course info
         	for (Course c : v) {
         		String newline = c.getTitle() + "\n";
 //        		System.out.println("Number slots" + c.getNumSlots());
@@ -520,7 +558,112 @@ public class Controller  implements Initializable{
         		textAreaConsole.setText(textAreaConsole.getText() + "\n" + newline);
         	}
     	}	
+    	
+    	// To draw the timetable if the enrollment is not yet implemented
+    	if (enrollbox.isEditable()==false) {
+    		drawtable();
+    	}
     }
+	
+	
+	@FXML
+	void drawtable() {
+		if (enrollbox.isEditable()==false) {
+			drawCourse.clear();
+			if (myCourseList.size()<5) {
+				drawCourse = myCourseList;
+			}
+			else {
+				for(int i = 0; i<5 ; i++) {
+					drawCourse.add(myCourseList.get(i));
+				}
+			}
+		}
+		// drawCourse.get(0).getSlot(0).getEnd().gethour returns INTEGER value
+		
+		//TODO Draw the table!
+    	//Add a random block on Saturday
+    	AnchorPane ap = (AnchorPane)tabTimetable.getContent();
+    	Label randomLabel = new Label("COMP1022\nL1");
+    	Label randomLabel2 = new Label("COMP3711\nL3");
+    	Random r = new Random();
+    	double start = (r.nextInt(2) + 1) * 20 + 40;
+    	
+    	// Use random, Random r = new Random().nextInt(1) to Color.color(red, green, blue), to have different color everytime.
+    	
+    	randomLabel2.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+
+    	randomLabel2.setLayoutX(500.0);
+    	randomLabel2.setLayoutY(start + 40);
+    	randomLabel2.setMinWidth(100.0);
+    	randomLabel2.setMaxWidth(100.0);
+    	randomLabel2.setMinHeight(60);
+    	randomLabel2.setMaxHeight(60);
+    	randomLabel2.setblendmode;
+    	String address = getParameter("adress");
+    	ap.getChildren().addAll(randomLabel2);
+	}
+	
+	
+	
+	
+		/*
+		// Add enrolled courses into the timetable as the normal case
+		else {
+			for (Course curr : filterCourse) {
+	    		if AmBox
+				Set<String> checkedID = new HashSet<String>();
+	    		String [] titleL = curr.getTitle().split("\\ -");
+	    		
+	    		for(int i=0;i<curr.getNumSlots();i++) {
+	    			Courselist currCour = new Courselist(titleL[0], curr.getSlot(i).getSectionCode(), titleL[1], curr.getSlot(i).getInstructor());
+	    			String CourRefID = titleL[0] + "--" + curr.getSlot(i).getSectionCode();
+//	    			textAreaConsole.setText(CourRefID+"2222222222222222222222");
+	    			
+	    			if (EnrolledCourse.contains(CourRefID)) {
+	    				currCour.getEnroll().setSelected(true);
+	    			}
+	    			
+	    	    	for(int k = 0; k < EnrolledCourse.size(); k++) {
+	    	    		System.out.println(EnrolledCourse.get(k));
+	    	    	}
+	    			
+
+	    			if(checkedID.contains(CourRefID) == false) {
+	    				checkedID.add(CourRefID);
+	    				tblist.add(currCour);
+	    				}    			     			   			
+	    			}
+			}
+		}
+	} 
+
+	    void enrollmentUpdate() {
+	    	
+	    	if (filterCourse == null) {
+	    		tblist.clear();
+	    		tblist.add(new Courselist("N/A","N/A","N/A","N/A"));
+	    		return;
+	    	}
+			//store the list of enrolled course
+	    	 
+	    	
+	    	// Checking and recording the enrollment status in the CourseListTable
+	    	
+	    	for (int i =0;i< CourseListTable.getItems().size();i++) {
+	    		String courseIDString = CourseListTable.getItems().get(i).getCourseCode() + "--" + CourseListTable.getItems().get(i).getSection();
+	    		if (CourseListTable.getItems().get(i).getEnroll().isSelected()) {
+	    			if(EnrolledCourse.contains(courseIDString) == false) {
+	    				EnrolledCourse.add(courseIDString);
+	    			}
+	    		}else {
+	    			if(EnrolledCourse.contains(courseIDString) == true) {
+	    				EnrolledCourse.remove(courseIDString);
+	    			}
+	    		}
+	    	}   
+	    }	
+	*/
 	
 
 	
--- src/main/java/comp3111/coursescraper/Courselist.java
+++ src/main/java/comp3111/coursescraper/Courselist.java
@@ -3,6 +3,7 @@
 package comp3111.coursescraper;
 
 import javafx.beans.Observable;
+
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
