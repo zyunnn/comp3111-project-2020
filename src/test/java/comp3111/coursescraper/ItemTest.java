b5bdfd7d82bf4c1d6c3925264b729ba1be9ea6aa
zhou zhuo rui <zzhouba@connect.ust.hk>
2020-05-08 21:47:05 +0800

    update for java doc

--- src/test/java/comp3111/coursescraper/ItemTest.java
+++ src/test/java/comp3111/coursescraper/ItemTest.java
@@ -7,9 +7,16 @@ import comp3111.coursescraper.Course;
 
 import static org.junit.Assert.*;
 
-
+/**
+ * Test if Course object items are normal
+ * @author zhou zhuo rui
+ *
+ */
 public class ItemTest {
 
+	/**
+	 * Test if title set and get function normally
+	 */
 	@Test
 	public void testSetTitle() {
 		Course i = new Course();
